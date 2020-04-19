37b362a63f1176d15cac9cf43f7be15c66d2f1f2
Crowdbotics <team@crowdbotics.com>
2020-04-19 13:38:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/frigomob1_16009/MainActivity.java
@@ -0,0 +1,15 @@
+package com.frigomob1_16009;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "frigomob1_16009";
+    }
+}
